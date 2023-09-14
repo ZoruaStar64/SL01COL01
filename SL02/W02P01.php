21e78a85bb6c6126847e9923cd75e9b492452834
ZoruaStar64 <sanderb119@gmail.com>
2023-09-14 11:40:21 +0200

    restyled the home page and finished Assignment 2
    Assignment 2 now displays the user info properly

--- SL02/W02P01.php
+++ SL02/W02P01.php
@@ -1,34 +1,55 @@
 <?php
 
 /* Included Files */
-include 'Functions/ageFunctions.php';
+include '../Functions/ageFunctions.php';
 
 /* Required Variables */
 $currentDate = date_create();
 
 /**
- * Multidimensionale array met gegevens van onze team PSR.
+ * Multidimensionale Associative array met gegevens van onze team PSR.
  */
 $PSR = [
-    ['first_name' => 'Sander', 'last_name' => 'Borgman', 'date_of_birth' => '30-01-2003',
-        'age' => calculateAge(date_create("30-01-2003"), $currentDate),
-        'e-mail' => 'sanderb119@gmail.com', 'phone_number' => '(06) 12345678',
+    'sander' => ['first_name' => 'Sander', 'last_name' => 'Borgman', 'date_of_birth' => '30-01-2003',
+        'age' => calculateAge(date_create("30-01-2003"), $currentDate), 'height' => 1.81,
+        'e-mail' => 's1198081@student.windesheim.nl', 'phone_number' => '(06) 12345678',
         'address' => 'someplace 456', 'hobbies' => ['Gaming', 'Watching videos'],
         'coding_languages' => ['HTML', 'CSS', 'JS', 'MySql', 'PHP', 'C#', 'Java', 'Lua'],
         'frameworks' => ['ReactJS', 'Laravel'], 'languages' => ['Dutch', 'English'],
         'graduations' => ['Software-Dev MBO-4, ROC Almere Buiten'], 'isEnrolled' => true],
-    ['first_name' => 'Paul', 'last_name' => 'van Breda', 'date_of_birth' => '30-01-2003',
-        'age' => calculateAge(date_create("30-01-2003"), $currentDate),
-        'e-mail' => 'placeholder1@gmail.com', 'phone_number' => '(06) 12345678',
+    'paul' => ['first_name' => 'Paul', 'last_name' => 'van Breda', 'date_of_birth' => '19-11-2001',
+        'age' => calculateAge(date_create("19-11-2001"), $currentDate), 'height' => 1.84,
+        'e-mail' => 's1200548@student.windesheim.nl', 'phone_number' => '(06) 87654321',
         'address' => 'someplace 789', 'hobbies' => ['hobby 1', 'hobby 2'],
         'coding_languages' => ['HTML', 'CSS', 'JS', 'MySql', 'PHP', 'Java',],
-        'frameworks' => [''], 'languages' => ['Dutch', 'English'], 'graduations' => ['Software-Dev MBO-4, ROC Almere Buiten'],
+        'frameworks' => ['placeholderframework'], 'languages' => ['Dutch', 'English'],
+        'graduations' => ['Software-Dev MBO-4, ROC Almere Buiten'],
         'isEnrolled' => true],
-    ['first_name' => 'Rick', 'last_name' => 'Beniers', 'date_of_birth' => '30-01-2003',
-        'age' => calculateAge(date_create("30-01-2003"), $currentDate),
-        'e-mail' => 'placeholder2@gmail.com', 'phone_number' => '(06) 12345678',
+    'rick' => ['first_name' => 'Rick', 'last_name' => 'Beniers', 'date_of_birth' => '30-01-2003',
+        'age' => calculateAge(date_create("30-01-2003"), $currentDate), 'height' => 1.81,
+        'e-mail' => 'placeholder@gmail.com', 'phone_number' => '(06) 56789123',
         'address' => 'someplace 123', 'hobbies' => ['hobby 1', 'hobby 2'],
         'coding_languages' => ['HTML', 'CSS', 'JS', 'MySql', 'PHP', 'C#', 'Java'],
-        'frameworks' => [''], 'languages' => ['Dutch', 'English', 'German'], 'graduations' => [''],
+        'frameworks' => ['placeholderframework'], 'languages' => ['Dutch', 'English', 'German'],
+        'graduations' => ['placeholdergraduation'],
         'isEnrolled' => true],
 ];
+
+/**
+ * This displays all the info in the team's respective arrays from the $PSR variable.
+ */
+foreach ($PSR as $member) {
+    foreach ($member as $memberInfo => $value) {
+        if (is_array($value)) {
+            echo $memberInfo . " = ";
+            foreach ($value as $arrayItem) {
+                echo $arrayItem . ', ';
+            }
+            echo '<br>';
+        }
+        else {
+            echo $memberInfo . " = " . $value . "<br>" ;
+        }
+    }
+    echo '<hr>';
+}
